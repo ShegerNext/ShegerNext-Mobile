7f6aeec83dd65debf66a877f1eaa6755ea13da3c
Rajaf Dereje <rajafgudeta@gmail.com>
2025-09-08 16:27:10 +0300

    add onboarding page

--- shegernext/lib/core/config/app_router.dart
+++ shegernext/lib/core/config/app_router.dart
@@ -1,7 +1,23 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:shegernext/core/config/route_names.dart';
+import 'package:shegernext/features/onboarding/presentation/screens/onboarding_page.dart';
+import 'package:shegernext/features/onboarding/presentation/screens/placeholder_home_page.dart';
 
 final GoRouter appRouter = GoRouter(
-  initialLocation: RouteNames.splash,
-  routes: []
+  initialLocation: RouteNames.onboarding,
+  routes: <RouteBase>[
+    GoRoute(
+      path: RouteNames.onboarding,
+      name: RouteNames.onboarding,
+      pageBuilder: (BuildContext context, GoRouterState state) =>
+          const MaterialPage(child: OnboardingPage()),
+    ),
+    GoRoute(
+      path: RouteNames.home,
+      name: RouteNames.home,
+      pageBuilder: (BuildContext context, GoRouterState state) =>
+          const MaterialPage(child: PlaceholderHomePage()),
+    ),
+  ],
 );
